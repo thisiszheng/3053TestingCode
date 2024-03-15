bcdc32dba94d06d9f74d146efd5375cbc05b99a4
thisiszheng <brianz12@nycstudents.net>
2024-03-15 18:42:35 -0400

    Added Climber

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,11 +5,13 @@
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -29,14 +31,22 @@ public class Robot extends TimedRobot {
 
   private Joystick joy1 = new Joystick(0);
 
+  public static final int leftMotorID = 15;
+  public static final int rightMotorID = 14;
+
+  CANSparkMax leftMotor = new CANSparkMax(leftMotorID, MotorType.kBrushless);
+  CANSparkMax rightMotor = new CANSparkMax(rightMotorID, MotorType.kBrushless);
+
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+  }
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+  }
 
   @Override
   public void autonomousPeriodic() {}
@@ -46,16 +56,37 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    double speed = -joy1.getRawAxis(1) * 0.3;
-    double turn = joy1.getRawAxis(4) * 0.3;
 
+    // DriveTrain Control
+    double speed = -joy1.getRawAxis(5) * 0.3;
+    double turn = joy1.getRawAxis(0) * 0.3;
+
+    // ClimbingArm Control
+    double Pull = joy1.getRawAxis(3) * 0.3;
+    double Push = joy1.getRawAxis(2) * 0.3;
+
+    // DriveTrain Varibles
     double left = speed + turn;
     double right = speed - turn;
 
+    // ClimbingArm Varible
+    double climbSpeed = Pull - Push;
+
+    /* Alternative Way
+     * rightMotor.follow(leftMotor);
+    */
+
+    // DriveTrain
     RightFront.set(ControlMode.PercentOutput, 0 + right);
     RightRear.set(ControlMode.PercentOutput, 0 + right);
     LeftFront.set(ControlMode.PercentOutput, 0 + left);
     LeftRear.set(ControlMode.PercentOutput, 0 + left);
+
+    // ClimbingArm
+    rightMotor.set(0 + climbSpeed);
+    leftMotor.set(0 + climbSpeed);
+
+    // when ClimbingArm is extending, Pull = 1
   }
 
   @Override
