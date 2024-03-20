0dbff3b08a9c6265574d5d9aeb2b6b6181b1e997
thisiszheng <brianz12@nycstudents.net>
2024-03-20 16:50:52 -0400

    + Encoder, PID, & SmartDashboard

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,8 +10,14 @@ import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkRelativeEncoder;
+import com.revrobotics.SparkPIDController;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -37,6 +43,10 @@ public class Robot extends TimedRobot {
   CANSparkMax leftMotor = new CANSparkMax(leftMotorID, MotorType.kBrushless);
   CANSparkMax rightMotor = new CANSparkMax(rightMotorID, MotorType.kBrushless);
 
+  RelativeEncoder climberEncoder;
+  SparkPIDController SparkPIDController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+
     // DriveTrain Varible & Control
     double speed = -driverJoystick.getRawAxis(5) * 0.3;
     double turn = driverJoystick.getRawAxis(0) * 0.3;
@@ -50,18 +60,51 @@ public class Robot extends TimedRobot {
     double Push = driverJoystick.getRawAxis(2) * 0.3;
 
     // ClimbingArm Calculation
-    double climbSpeed = Pull - Push;
-
+    double climbSpeed = Pull - Push; // when ClimbingArm is extending, Pull = 1
+    
   @Override
   public void robotInit() {
+    climberEncoder = leftMotor.getEncoder(SparkRelativeEncoder.Type.kQuadrature, 4096);
+
+    leftMotor.restoreFactoryDefaults();
+
+    SparkPIDController = leftMotor.getPIDController();
+
+    SparkPIDController.setFeedbackDevice(climberEncoder);
+
+    // PID coefficients
+    kP = 0;
+    kI = 1e-4;
+    kD = 1;
+    kIz = 0;
+    kFF = 0;
+    kMaxOutput = 1;
+    kMinOutput = -1;
+
+    // set PID coefficients
+    SparkPIDController.setP(kP);
+    SparkPIDController.setI(kI);
+    SparkPIDController.setD(kD);
+    SparkPIDController.setIZone(kIz);
+    SparkPIDController.setFF(kFF);
+    SparkPIDController.setOutputRange(kMinOutput, kMaxOutput);
+
+    // display PID coefficients on SmartDashboard
+    SmartDashboard.putNumber("P Gain", kP);
+    SmartDashboard.putNumber("I Gain", kI);
+    SmartDashboard.putNumber("D Gain", kD);
+    SmartDashboard.putNumber("I Zone", kIz);
+    SmartDashboard.putNumber("Feed Forward", kFF);
+    SmartDashboard.putNumber("Max Output", kMaxOutput);
+    SmartDashboard.putNumber("Min Output", kMinOutput);
+    SmartDashboard.putNumber("Set Rotations", 0);
   }
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {
-  }
+  public void autonomousInit() {}
 
   @Override
   public void autonomousPeriodic() {}
@@ -82,7 +125,45 @@ public class Robot extends TimedRobot {
     rightMotor.set(0 + climbSpeed);
     leftMotor.set(0 + climbSpeed);
 
-    // when ClimbingArm is extending, Pull = 1
+        // read PID coefficients from SmartDashboard
+    double p = SmartDashboard.getNumber("P Gain", 0);
+    double i = SmartDashboard.getNumber("I Gain", 0);
+    double d = SmartDashboard.getNumber("D Gain", 0);
+    double iz = SmartDashboard.getNumber("I Zone", 0);
+    double ff = SmartDashboard.getNumber("Feed Forward", 0);
+    double max = SmartDashboard.getNumber("Max Output", 0);
+    double min = SmartDashboard.getNumber("Min Output", 0);
+    double rotations = SmartDashboard.getNumber("Set Rotations", 0);
+
+    // if PID coefficients on SmartDashboard have changed, write new values to controller
+    if((p != kP)) { SparkPIDController.setP(p); kP = p; }
+    if((i != kI)) { SparkPIDController.setI(i); kI = i; }
+    if((d != kD)) { SparkPIDController.setD(d); kD = d; }
+    if((iz != kIz)) { SparkPIDController.setIZone(iz); kIz = iz; }
+    if((ff != kFF)) { SparkPIDController.setFF(ff); kFF = ff; }
+    if((max != kMaxOutput) || (min != kMinOutput)) { 
+      SparkPIDController.setOutputRange(min, max); 
+      kMinOutput = min; kMaxOutput = max; 
+    }
+
+    /**
+     * PIDController objects are commanded to a set point using the 
+     * SetReference() method.
+     * 
+     * The first parameter is the value of the set point, whose units vary
+     * depending on the control type set in the second parameter.
+     * 
+     * The second parameter is the control type can be set to one of four 
+     * parameters:
+     *  com.revrobotics.CANSparkMax.ControlType.kDutyCycle
+     *  com.revrobotics.CANSparkMax.ControlType.kPosition
+     *  com.revrobotics.CANSparkMax.ControlType.kVelocity
+     *  com.revrobotics.CANSparkMax.ControlType.kVoltage
+     */
+    SparkPIDController.setReference(rotations, CANSparkMax.ControlType.kPosition);
+    
+    SmartDashboard.putNumber("SetPoint", rotations);
+    SmartDashboard.putNumber("ProcessVariable", climberEncoder.getPosition());
   }
 
   @Override
